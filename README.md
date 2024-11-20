df66459abe082c78155b94abe3a8392959ee8b11
Andrew <adngo@ucdavis.edu>
2024-11-19 23:34:14 -0800

    Added LCD and Pot to Circuit, Updated README

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Catabed
 A mod to motorized wheelchairs to allow for easier access to a bed
+
+You need to have [this](https://reference.arduino.cc/reference/en/libraries/liquidcrystal-i2c/) library included in your sketch for the code to compile
--- wheelchair/wheelchair.ino
+++ wheelchair/wheelchair.ino
@@ -1,13 +1,19 @@
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
 #include <Servo.h>
 
 // Servo
 Servo mainServo;
+
+// LCD
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 // Pins
 const int trigPin = 9;
 const int echoPin = 10;
 const int buttonPin = 7;
 const int servoPin = 6;
+const int potPin = A0;
 
 // Functions
 double getDistance(int trigPin, int echoPin) {
@@ -34,42 +40,78 @@ void slowlyRotate(int inputAngle, int buttonPin, bool obstructionCheck, Servo *s
     for(int i = 1; i <= (inputAngle - currentAngle); i++) {
       (*servo).write(currentAngle + i);
       if(checkObstruction(trigPin, echoPin, .10) == 0 && obstructionCheck) break;
-      delay(10);
+      delay(30);
     }
   } else if(currentAngle > inputAngle) {
     for(int i = 1; i <= (currentAngle - inputAngle); i++) {
       (*servo).write(currentAngle - i);
       if(checkObstruction(trigPin, echoPin, .10) == 0 && obstructionCheck) break;
-      delay(10);
+      delay(30);
     }
   }
   
   return;
 }
 
+void updateLCD(int setAngle, LiquidCrystal_I2C *lcd, Servo *servo) {
+  (*lcd).clear();
+
+  // Set current angle number
+  (*lcd).setCursor(0, 0);
+  (*lcd).print("Current: ");
+  (*lcd).print((*servo).read());
+
+  // Set set angle number
+  (*lcd).setCursor(0, 1);
+  (*lcd).print("Set: ");
+  (*lcd).print(setAngle);
+
+  return;
+}
+
 // Main
 void setup() {
+  // Pin Setup
   pinMode(trigPin, OUTPUT);  
   pinMode(echoPin, INPUT);
   pinMode(buttonPin, INPUT);
+
+  // Attach motor
   mainServo.attach(servoPin);
+
+  // Turn on LCD
+  lcd.init();       
+  lcd.backlight();  
+  updateLCD(map(analogRead(potPin), 0, 1023, 45, 90), &lcd, &mainServo);
+
+  // Turn on serial
   Serial.begin(9600);    
 }
 
 void loop() {
+  static int prevSetAngle = -1;
+  static int setAngle = map(analogRead(potPin), 0, 1023, 45, 90);
+  
+  setAngle = map(analogRead(potPin), 0, 1023, 45, 90);
+  
   if(digitalRead(buttonPin) == 1) {
     if(mainServo.read() != 90) {
-      slowlyRotate(90, buttonPin, false, &mainServo);
+      if(setAngle != mainServo.read()) {
+        slowlyRotate(setAngle, buttonPin, false, &mainServo); 
+      } else {
+        slowlyRotate(90, buttonPin, false, &mainServo); 
+      }
       delay(500);
-      Serial.println(mainServo.read());
     } else if(checkObstruction(trigPin, echoPin, .10)) {
       if(mainServo.read() == 90) {
-        slowlyRotate(45, buttonPin, true, &mainServo);
+        slowlyRotate(setAngle, buttonPin, true, &mainServo);
         delay(500);
-        Serial.println(mainServo.read());
       }
     }
+    updateLCD(setAngle, &lcd, &mainServo);
   }
   
+  if(prevSetAngle != setAngle) updateLCD(setAngle, &lcd, &mainServo);
+  prevSetAngle = setAngle;
   delay(10);
 }
